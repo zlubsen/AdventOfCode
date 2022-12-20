72445b69a0ddc9d262c511137199197c128a5c5f
Zeeger <zeeger@lubsen.eu>
2022-12-20 21:29:07 +0100

    Day 8 pt 2

--- aoc-2019-rust/src/bin/day8.rs
+++ aoc-2019-rust/src/bin/day8.rs
@@ -27,9 +27,13 @@ fn part1() {
 
 fn part2() {
     let input = read_input("inputs/day8.txt");
-    // let layers = parse_layers(25 * 6, input.as_str());
+    let image = render(input.clone(), 25*6);
+    print_image(image, 25);
 
-    // let
+    // Attempt where we create vec of columns throught the layers. Produced wrong image.
+    // let decoded = decode(25,6, input.as_str());
+    // let fused = fuse(&decoded);
+    // print_image_2(fused);
 }
 
 fn parse_layers(layer_size: usize, input: &str) -> Vec<Vec<char>> {
@@ -63,35 +67,90 @@ fn ones_by_twos(layers: &Vec<Vec<char>>) -> usize {
     ones_by_twos
 }
 
+fn render(image: String, layer_size: usize) -> Vec<char> {
+    image
+        .as_bytes()
+        .chunks(layer_size)
+        .fold(vec![' '; layer_size], |display, layer| {
+            (0..layer_size)
+                .map(|i| {
+                    if display[i] == ' ' {
+                        match layer[i] {
+                            48 => '░',
+                            49 => '█',
+                            50 => ' ',
+                            c => panic!("Unrecognized character: {}", c),
+                        }
+                    }
+                    else {
+                        display[i]
+                    }
+                })
+                .collect()
+        })
+}
+
+fn print_image(image: Vec<char>, width: usize) {
+    image.chunks(width).for_each(|row| {
+        for (i, pix) in row.iter().enumerate() {
+            print!("{pix}");
+            if (i+1) % width == 0 {
+                print!("\n");
+            }
+        }
+    });
+}
+
 fn decode(width: usize, height: usize, input: &str) -> Vec<Vec<Vec<char>>> {
     let chars : Vec<char> = input.chars().collect();
     let layer_size = width*height;
     let num_layers = input.len()/layer_size;
 
-    let mut layers: Vec<Vec<Vec<char>>> = Vec::new();
-    for layer_idx in 0..num_layers {
-        let mut rows: Vec<Vec<char>> = Vec::new();
-        for row_idx in 0..width {
-            let mut cols: Vec<char> = Vec::new();
-            for col_idx in 0..height {
+    let mut rows: Vec<Vec<Vec<char>>> = Vec::new();
+    for row_idx in 0..height {
+        let mut cols: Vec<Vec<char>> = Vec::new();
+        for col_idx in 0..width {
+            let mut layers: Vec<char> = Vec::new();
+            for layer_idx in 0..num_layers {
                 let index = (layer_idx * (layer_size)) + (row_idx * (height)) + (col_idx);
                 let char = chars.get(index).unwrap();
-                cols.push(char.clone());
+                layers.push(char.clone());
             }
-            rows.push(cols);
+            cols.push(layers);
         }
-        layers.push(rows);
+        rows.push(cols);
     }
-    layers
+    rows
 }
 
-fn stack(layers: &Vec<Vec<Vec<char>>>) -> Vec<Vec<char>> {
-    layers.iter().map(|layer|  )
+fn fuse(rows: &Vec<Vec<Vec<char>>>) -> Vec<Vec<&char>> {
+    let mut fused_row = Vec::new();
+    for row in rows {
+        let mut fused_col = Vec::new();
+        for col in row {
+            let pixel : Vec<&char> = col.iter().filter(|&&pix| pix != '2').collect();
+            let pixel = pixel.first().unwrap();
+            fused_col.push(pixel.clone());
+        }
+        fused_row.push(fused_col);
+    }
+    fused_row
+}
+
+fn print_image_2(image: Vec<Vec<&char>>) {
+    image.iter().for_each(|row| {
+        let line = row.iter().map(|&char| {
+            if char == &'0' {
+                ' '
+            } else { '#' }
+        }).join("");
+        println!("{line}");
+    });
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{decode, ones_by_twos, parse_layers};
+    use crate::{ones_by_twos, parse_layers, print_image, render};
 
     #[test]
     fn test_pt1() {
@@ -104,9 +163,13 @@ mod tests {
     #[test]
     fn test_pt2() {
         let input = "0222112222120000";
-        let decoded = decode(2,2, input);
-        assert_eq!(decoded, vec![vec![vec!['0', '2'], vec!['2', '2']], vec![vec!['1', '1'], vec!['2', '2']], vec![vec!['2', '2'], vec!['1', '2']], vec![vec!['0', '0'], vec!['0', '0']]]);
-        let stacked = stack(&decoded);
-        assert_eq!(stacked, vec![vec!['0', '1'], vec!['1','0']])
+        let image = render(input.to_string(), 2*2);
+        assert_eq!(image, vec!['░','█','█','░']);
+        print_image(image, 2);
+        // let decoded = decode(2,2, input);
+        // assert_eq!(decoded, vec![vec![vec!['0', '1', '2', '0'], vec!['2', '1', '2', '0']], vec![vec!['2', '2', '1', '0'], vec!['2', '2', '2', '0']]]);
+        // let fused = fuse(&decoded);
+        // assert_eq!(fused, vec![vec![&'0', &'1'], vec![&'1', &'0']]);
+        // print_image(fused);
     }
 }
