60fff397c528d9786ca280e2db8e05c0fb06cae3
Zeeger <zeeger@lubsen.eu>
2022-12-25 22:05:32 +0100

    Day 10 start

---
+++ aoc-2019-rust/src/bin/day10.rs
@@ -0,0 +1,28 @@
+use std::time::Instant;
+use aoc_2019_rust::read_input;
+
+fn main() {
+    println!("Part 1");
+    let start_1 = Instant::now();
+    part1();
+    let duration_1 = start_1.elapsed();
+    println!("- took {} micro secs", duration_1.as_micros());
+
+    println!();
+
+    println!("Part 2");
+    let start_2 = Instant::now();
+    part2();
+    let duration_2 = start_2.elapsed();
+    println!("- took {} micro secs", duration_2.as_micros());
+}
+
+fn part1() {
+    let input = read_input("inputs/day10.txt");
+    let rows: Vec<&str> = input.split("\n").collect();
+    println!("{rows:?}");
+}
+
+fn part2() {
+    // let input = read_input("inputs/day9.txt");
+}
