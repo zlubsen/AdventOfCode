ecdd219ca7367e0d67277992664ced79956ce654
Zeeger <zeeger@lubsen.eu>
2022-12-28 11:16:58 +0100

    Day 10 pt 1

--- aoc-2019-rust/src/bin/day10.rs
+++ aoc-2019-rust/src/bin/day10.rs
@@ -1,19 +1,8 @@
-use std::cmp::Ordering;
 use std::time::Instant;
 use itertools::Itertools;
 use aoc_2019_rust::read_input;
 
 type Coordinate = (usize, usize);
-type Vector = (i32, i32);
-type NormalVector = (f32, f32);
-
-const NUM_DEC_ROUND : f32 = 100000.0;
-
-fn round_vec(vec: NormalVector) -> NormalVector {
-    let x = (vec.0 * NUM_DEC_ROUND).round() / NUM_DEC_ROUND;
-    let y = (vec.1 * NUM_DEC_ROUND).round() / NUM_DEC_ROUND;
-    (x,y)
-}
 
 fn main() {
     println!("Part 1");
@@ -36,6 +25,7 @@ fn part1() {
     let map: Vec<(usize, usize)> = parse_map(&input);
     let visibles = visibles_per_asteroid(map);
     let highest = highest_visible(visibles);
+    println!("{highest}");
 }
 
 fn part2() {
@@ -56,27 +46,14 @@ fn parse_map(input: &str) -> Vec<Coordinate> {
 }
 
 fn visibles_per_asteroid(map: Vec<Coordinate>) -> Vec<usize> {
-    let visibles: Vec<usize> = map.iter()
-        .map(|coord| map.iter()
-            .map(|other| distance_vector(*coord, *other))
-            .filter(|me| me != &(0,0))
-            .map(|vector| normalize_vector(&vector))
-            .map(|v| round_vec(v))
-            .sorted_by(|a, b| {
-                let len_a = (a.0.powi(2) + a.1.powi(2)).sqrt();
-                let len_b = (b.0.powi(2) + b.1.powi(2)).sqrt();
-                if len_a < len_b {
-                    Ordering::Less
-                } else if a.0 == b.0 && a.1 == b.1 {
-                    Ordering::Equal
-                } else {
-                    Ordering::Greater
-                }
-            }).dedup()
-            .collect::<Vec<NormalVector>>())
-        .map(|normals| normals.len())
-        .collect();
-    visibles
+    let count = map.iter().map(|coordinate| map.iter()
+        .filter(|&other| coordinate != other )
+        .map(|other| angle(&coordinate, &other))
+        .sorted_by(f32::total_cmp)
+        .dedup().collect::<Vec<f32>>())
+        .map(|asteroids| asteroids.len())
+        .collect::<Vec<usize>>();
+    count
 }
 
 fn highest_visible(visibles: Vec<usize>) -> usize {
@@ -84,25 +61,21 @@ fn highest_visible(visibles: Vec<usize>) -> usize {
     **(aa.first().unwrap())
 }
 
-fn distance_vector(a: Coordinate, b: Coordinate) -> Vector {
-    let a_x = a.0 as i32;
-    let a_y = a.1 as i32;
-    let b_x = b.0 as i32;
-    let b_y = b.1 as i32;
+fn angle(a: &Coordinate, b: &Coordinate) -> f32 {
+    let x = b.0 as f32 - a.0 as f32;
+    let y = b.1 as f32 - a.1 as f32;
+    let degrees = y.atan2(x).to_degrees() + 90.0;
 
-    (b_x - a_x, b_y - a_y)
-}
-
-fn normalize_vector(vector: &Vector) -> NormalVector {
-    let determinant = ((vector.0.pow(2) + vector.1.pow(2)) as f32).sqrt();
-    let x = vector.0 as f32/determinant;
-    let y = vector.1 as f32/determinant;
-    (x, y)
+    if degrees < 0.0 {
+        degrees + 360.0
+    } else {
+        degrees
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{distance_vector, highest_visible, normalize_vector, parse_map, visibles_per_asteroid};
+    use crate::{angle, highest_visible, parse_map, visibles_per_asteroid};
 
     #[test]
     fn test_parse_map() {
@@ -119,46 +92,43 @@ mod tests {
     }
 
     #[test]
-    fn test_distance_vector_q1() {
+    fn test_angle_q1() {
         let a = (2usize, 2usize);
         let b = (0usize, 0usize);
-        let dist = distance_vector(a, b);
-        assert_eq!(dist, (-2,-2));
+        let angle = angle(&a, &b);
+        assert_eq!(angle, 315.0);
     }
 
     #[test]
-    fn test_distance_vector_q2() {
+    fn test_angle_q2() {
         let a = (2usize, 2usize);
         let b = (4usize, 0usize);
-        let dist = distance_vector(a, b);
-        assert_eq!(dist, (2,-2));
+        let angle = angle(&a, &b);
+        assert_eq!(angle, 45.0);
     }
 
     #[test]
-    fn test_distance_vector_q3() {
-        let a = (0usize, 0usize);
-        let b = (2usize, 2usize);
-        let dist = distance_vector(a, b);
-        assert_eq!(dist, (2,2));
+    fn test_angle_q3() {
+        let a = (2usize, 2usize);
+        let b = (4usize, 4usize);
+        let angle = angle(&a, &b);
+        assert_eq!(angle, 135.0);
     }
 
     #[test]
-    fn test_distance_vector_q4() {
+    fn test_angle_q4() {
         let a = (2usize, 2usize);
         let b = (0usize, 4usize);
-        let dist = distance_vector(a, b);
-        assert_eq!(dist, (-2,2));
+        let angle = angle(&a, &b);
+        assert_eq!(angle, 225.0);
     }
 
     #[test]
-    fn test_normalize_vector() {
-        let normal_1 = normalize_vector(&(2,2));
-        assert_eq!(normal_1, ( 2f32.sqrt()/2f32 , 2f32.sqrt()/2f32 ));
-
-        let normal_2 = normalize_vector(&(4,4));
-        assert_eq!(normal_2, ( 2f32.sqrt()/2f32 , 2f32.sqrt()/2f32 ));
-
-        assert_eq!(normal_1, normal_2);
+    fn test_angle_self() {
+        let a = (2usize, 2usize);
+        let b = (2usize, 2usize);
+        let angle = angle(&a, &b);
+        assert_eq!(angle, 90.0);
     }
 
     #[test]
